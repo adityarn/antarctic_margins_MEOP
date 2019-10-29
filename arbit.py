changes made to wind analysis correcting the wind stress curl computation

arbit.py
@@ -1,17 +1,47 @@
-zlowest = timeSlice.DEPTH.min() 
-number_bins = np.abs(zlowest) // zbin
-zbin_exact = np.abs(zlowest) / float(number_bins)
-depth_bins = np.linspace(zlowest-1, 0, number_bins)
+plt.close(1)
+plt.figure(1, figsize=(190/25.4, 230/25.4) )
 
-DSW = (timeSlice.gamman > 28.27) & (timeSlice.PSAL_ADJUSTED > 34.5) & (timeSlice.CTEMP <= -1.8) & (timeSlice.CTEMP >= -1.9)
-lssw = (timeSlice.PSAL_ADJUSTED >= 34.3) & (timeSlice.PSAL_ADJUSTED <= 34.4) & (timeSlice.CTEMP <= -1.5) & (timeSlice.CTEMP > -1.9)
-ISW = (timeSlice.CTEMP < -1.9)
-CDW = (timeSlice.CTEMP >= 0) & (timeSlice.PSAL_ADJUSTED >= 34.5)
-mCDW = (timeSlice.CTEMP < 0) & (timeSlice.CTEMP > -1.8) & (timeSlice.gamman > 28) & (timeSlice.gamman < 28.27)
+mapax = plt.subplot(projection = ccrs.Orthographic(central_longitude=0, central_latitude=-90) )
 
-totalcount = timeSlice.groupby(pd.cut(timeSlice.DEPTH, depth_bins) ).CTEMP.count().values
-DSWcount = timeSlice.loc[DSW].groupby(pd.cut(timeSlice.loc[DSW].DEPTH, depth_bins) ).CTEMP.count().values
-LSSWcount = timeSlice.loc[lssw].groupby(pd.cut(timeSlice.loc[lssw].DEPTH, depth_bins) ).CTEMP.count().values
-ISWcount = timeSlice.loc[ISW].groupby(pd.cut(timeSlice.loc[ISW].DEPTH, depth_bins) ).CTEMP.count().values
-mCDWcount = timeSlice.loc[mCDW].groupby(pd.cut(timeSlice.loc[mCDW].DEPTH, depth_bins) ).CTEMP.count().values
-CDWcount = timeSlice.loc[CDW].groupby(pd.cut(timeSlice.loc[CDW].DEPTH, depth_bins) ).CTEMP.count().values
+DATADIR = "/media/data"
+
+shpfile = DATADIR+"/Datasets/Shapefiles/AntarcticGroundingLine/GSHHS_f_L6.shp"
+with fiona.open(shpfile) as records:
+    geometries = [sgeom.shape(shp['geometry']) for shp in records]
+ISedgefname = DATADIR+"/Datasets/Shapefiles/AntIceShelf/ne_10m_antarctic_ice_shelves_polys.shp"
+ISe_feature = ShapelyFeature(Reader(ISedgefname).geometries(), 
+                             ccrs.PlateCarree(), linewidth=0.2,
+                             facecolor='none', 
+                             edgecolor="k")
+
+mapax.add_geometries(geometries, ccrs.PlateCarree(), edgecolor='0.25', facecolor='0.7',alpha=0.25, linewidth=0.2)
+mapax.add_feature(ISe_feature, zorder=3)
+
+
+gl = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, zorder=2,
+              linewidth=0.5, color='gray', alpha=1, linestyle='--')
+
+gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 20))
+gl.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5))
+gl.xformatter = LONGITUDE_FORMATTER
+gl.yformatter = LATITUDE_FORMATTER
+
+sel_inds = dfmg.loc[sel_tags].groupby("PROFILE_NUMBER").head(1).index
+lons = dfmg.loc[sel_inds, "LONGITUDE"].unique()
+lats = dfmg.loc[sel_inds, "LATITUDE"].unique()
+
+
+gl_regions = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle=':', zorder=3)
+#gl_regions_lowZ = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle='--', zorder=1)
+gl_regions.xlocator = mticker.FixedLocator(region_lons)
+gl_regions.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5) )
+gl_regions.xformatter = LONGITUDE_FORMATTER
+gl_regions.yformatter = LATITUDE_FORMATTER
+gl_regions.ylines = False
+
+
+if(plotBathy == True):
+    bathyS = xr.open_dataset('/media/hdd2/SOSE_1_12/bathyS.nc')
+    cs = mapax.contour(bathyS.lon, bathyS.lat, bathyS.elevation.where(bathyS.elevation <= 0).values,  levels=[-1000], colors="b", linestyle=":", linewidths=0.25, transform = ccrs.PlateCarree())
+    
+mapax.scatter(lons, lats, marker="x", color="b")

plot_topView_contourf.py
@@ -967,19 +967,23 @@ def plotDataDensity_NIS_DIS(df1, df2, units='Data Density', save=False, savename
     mapax.add_geometries(geometries, ccrs.PlateCarree(), edgecolor='0.25', facecolor='0.7',alpha=0.25, linewidth=0.2)
     mapax.add_feature(ISe_feature, zorder=3)
 
-    if region_lons:
-        gl_regions = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='gray', alpha=1, linestyle='--', zorder=3)
-        gl_regions.xlocator = mticker.FixedLocator(region_lons)
-        gl_regions.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5) )
-        gl_regions.xformatter = LONGITUDE_FORMATTER
 
-    gl = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, zorder=3,
+    gl = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, zorder=2,
                   linewidth=0.5, color='gray', alpha=1, linestyle='--')
     
     gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 20))
     gl.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5))
     gl.xformatter = LONGITUDE_FORMATTER
-    gl.yformatter = LATITUDE_FORMATTER        
+    gl.yformatter = LATITUDE_FORMATTER
+
+    if region_lons:
+        gl_regions = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle=':', zorder=3)
+        #gl_regions_lowZ = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle='--', zorder=1)
+        gl_regions.xlocator = mticker.FixedLocator(region_lons)
+        gl_regions.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5) )
+        gl_regions.xformatter = LONGITUDE_FORMATTER
+        gl_regions.yformatter = LATITUDE_FORMATTER
+        gl_regions.ylines = False
     
     matplotlib.rcParams.update({'font.size': fontsize})    
     
@@ -1013,7 +1017,7 @@ def plotDataDensity_NIS_DIS(df1, df2, units='Data Density', save=False, savename
         cmap = LinearSegmentedColormap.from_list(name='linearCmap', colors=color_scheme[1], N=len(levels)-1) 
 
         CF.append(mapax.pcolormesh(XX, YY, ni_masked, vmin=min(levels), vmax=max(levels), norm=bnorm, 
-                                   cmap=cmap, transform = ccrs.PlateCarree(), zorder=2))
+                                   cmap=cmap, transform = ccrs.PlateCarree(), zorder=1))
     
     
     if(plotBathy == True):

plot_waterMassThickness.py
@@ -991,6 +991,11 @@ def plot_WaterMass_Correlation_byRegion_CI(waterMassThickness, regionsName=[], t
     wth = 0.1
     axarr = []
     count = 0
+
+    shaded_x, shaded_y = np.meshgrid(np.linspace(0,3.5, 100),  np.linspace(-1,1,10))
+    shading = np.ones_like(shaded_x)
+    shading = ma.masked_where((shaded_x > 1.5) & (shaded_x < 2.5), shading)
+    
     for i in range(mrows):
         for j in range(ncols):
             axarr.append(plt.subplot(gs[i,j]))
@@ -1012,8 +1017,10 @@ def plot_WaterMass_Correlation_byRegion_CI(waterMassThickness, regionsName=[], t
             axarr[-1].axhline(y=0)
             axarr[-1].set_title(titles[count])
             axarr[-1].set_xticks([1, 2, 3])
+            axarr[-1].set_xlim(0.5,3.5)
+            axarr[-1].pcolormesh(shaded_x, shaded_y, shading, cmap="Greys", vmin=0, vmax=7)
             if(i == mrows-1):
-                axarr[-1].set_xticklabels(["-U$_{Slope}$", "", "V", "", "$-(\\nabla \\times \\tau)_{slope}$"], rotation=0)
+                axarr[-1].set_xticklabels(["-U$_{Slope}$", "V", "$-(\\nabla \\times \\tau)_{slope}$"], rotation=0)
             else:
                 axarr[-1].set_xticklabels([])
                 axarr[-1].set_xlabel("")
@@ -1027,7 +1034,7 @@ def plot_WaterMass_Correlation_byRegion_CI(waterMassThickness, regionsName=[], t
                 axarr[-1].set_ylabel("")
             axarr[-1].set_ylim(-1, 1)
             axarr[-1].set_yticks(np.arange(-1,1.1,0.25))
-            axarr[-1].grid(linestyle=":")
+            axarr[-1].grid(linestyle=":", axis="y")
             count+=1
     
     handles, labels = axarr[-1].get_legend_handles_labels()