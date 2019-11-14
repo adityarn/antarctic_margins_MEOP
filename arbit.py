66bd0c6d0c0cc6917dc9b3e648b98c7b721981e0
Aditya Narayanan <adityarn@gmail.com>
2019-11-14 17:55:11 +0530

    added wind ERA5 analysis notebook

--- arbit.py
+++ arbit.py
@@ -1,77 +1,55 @@
-def compute_stress_curl(winds):
-    stress_curl = np.zeros_like(winds.iews)
-    wek = np.zeros_like(winds.iews)
-    rho0 = 1025.0
-    tauy_x = np.zeros_like(winds.iews)
-    taux_y = np.zeros_like(winds.iews)
+def plot_fields_orthographic(field, longitude_coord, latitude_coord, vmin, vmax, save=False, savename="untitled.png",
+                             colorbar_label="", region_lons=None):
+    DATADIR = "/media/data"
+    plt.close(1)
+    plt.figure(1, figsize=(7,7) )
+    gs = gridspec.GridSpec(5, 2, width_ratios=[1, 0.05], wspace=0.05)
+
+    mapax = plt.subplot(gs[:,0], projection = ccrs.Orthographic(central_longitude=0, central_latitude=-90) )
+    CF = mapax.pcolormesh(longitude_coord, latitude_coord, 
+                          field, transform=ccrs.PlateCarree(), cmap='RdBu_r', vmin = -8, vmax = 8)
+    mapax.coastlines()
+
+    gl = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, zorder=2,
+                  linewidth=0.5, color='gray', alpha=1, linestyle='--')
     
-    delta_lat = abs(float(winds.latitude[0] - winds.latitude[1]))
-    delta_lon = abs(float(winds.longitude[0] - winds.longitude[1]))
-    r = 6371e3 # radius of earth in metres
-    dy = float(r * np.deg2rad(delta_lat))
-    print(dy)
-    omega = 2*np.pi/(24.*3600)
-    f = 2. * omega * np.sin(np.deg2rad(winds.latitude.values))
-    time_counter = 0
-    for t in winds.time:
-    
-        lat_counter = 0
-        for lat in winds.latitude:
-            
-            dx = float(r * np.cos(np.deg2rad(lat) ) * np.deg2rad(delta_lon))
-            
-            tauy_x[time_counter, lat_counter] = np.gradient(winds.sel(time = t, latitude=lat).inss , dx)
-            lat_counter+=1
-        taux_y[time_counter], _ = np.gradient(winds.sel(time = t).iews , -dy, dx)
-        stress_curl[time_counter] = tauy_x[time_counter] - taux_y[time_counter]
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 20))
+    gl.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5))
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+
+    if region_lons:
+        gl_regions = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle=':', zorder=3)
+        #gl_regions_lowZ = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle='--', zorder=1)
+        gl_regions.xlocator = mticker.FixedLocator(region_lons)
+        gl_regions.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5) )
+        gl_regions.xformatter = LONGITUDE_FORMATTER
+        gl_regions.yformatter = LATITUDE_FORMATTER
+        gl_regions.ylines = False
+
+    shpfile = DATADIR+"/Datasets/Shapefiles/AntarcticGroundingLine/GSHHS_f_L6.shp"
+    with fiona.open(shpfile) as records:
+        geometries = [sgeom.shape(shp['geometry']) for shp in records]
+    ISedgefname = DATADIR+"/Datasets/Shapefiles/AntIceShelf/ne_10m_antarctic_ice_shelves_polys.shp"
+    ISe_feature = ShapelyFeature(Reader(ISedgefname).geometries(), 
+                                 ccrs.PlateCarree(), linewidth=0.2,
+                                 facecolor='none', 
+                                 edgecolor="k")
+
+    mapax.add_geometries(geometries, ccrs.PlateCarree(), edgecolor='0.25', facecolor='0.7',alpha=0.25, linewidth=0.2)
+    mapax.add_feature(ISe_feature, zorder=3)
+
+    for l in np.arange(-160, 181, 20):
+        if( (l == 80) or (l == 100) ):
+            text_lat = -80
+        else:
+            text_lat  = -62.5
+        mapax.text(l, text_lat, str(l)+"$^{\circ}$", transform=ccrs.PlateCarree() )    
         
-        for l in range(len(winds.latitude)):
-            wek[time_counter, l] = stress_curl[time_counter, l] / (f[l] * rho0)
-        time_counter += 1
-    
-    print(dx)
-    windEk = xr.Dataset({'stressCurl':(['time', 'latitude', 'longitude'], stress_curl), 
-                         'wek':(['time', 'latitude', 'longitude'], wek) }, 
-                        coords={'time': winds.time, 'latitude': winds.latitude, 
-                               'longitude': winds.longitude})
-    return windEk
+    colorbar_ax = plt.subplot(gs[1:-1, 1])
+    Colorbar(mappable = CF, ax = colorbar_ax)
+    colorbar_ax.set_ylabel(colorbar_label)
 
-
-
-def compute_clim_stress_curl_the_WRONG_way(winds_monthlyMean):
-    stress_curl = np.zeros_like(winds_monthlyMean.iews)
-    wek = np.zeros_like(winds_monthlyMean.iews)
-    rho0 = 1025.0
-    tauy_x = np.zeros_like(winds_monthlyMean.iews)
-    taux_y = np.zeros_like(winds_monthlyMean.iews)
-    
-    delta_lat = abs(float(winds_monthlyMean.latitude[0] - winds_monthlyMean.latitude[1]))
-    delta_lon = abs(float(winds_monthlyMean.longitude[0] - winds_monthlyMean.longitude[1]))
-    r = 6371e3 # radius of earth in metres
-    dy = float(r * np.deg2rad(delta_lat))
-    print(dy)
-    omega = 2*np.pi/(24.*3600)
-    f = 2. * omega * np.sin(np.deg2rad(winds_monthlyMean.latitude.values))
-    time_counter = 0
-    for m in winds_monthlyMean.month:
-    
-        lat_counter = 0
-        for lat in winds_monthlyMean.latitude:
-            
-            dx = float(r * np.cos(np.deg2rad(lat) ) * np.deg2rad(delta_lon))
-            
-            tauy_x[time_counter, lat_counter] = np.gradient(winds_monthlyMean.sel(month = m, latitude=lat).inss , dx)
-            lat_counter+=1
-        taux_y[time_counter], _ = np.gradient(winds_monthlyMean.sel(time = t).iews , -dy, dx)
-        stress_curl[time_counter] = tauy_x[time_counter] - taux_y[time_counter]
-        
-        for l in range(len(winds_monthlyMean.latitude)):
-            wek[time_counter, l] = stress_curl[time_counter, l] / (f[l] * rho0)
-        time_counter += 1
-    
-    print(dx)
-    windEk = xr.Dataset({'stressCurl':(['time', 'latitude', 'longitude'], stress_curl), 
-                         'wek':(['time', 'latitude', 'longitude'], wek) }, 
-                        coords={'time': winds_monthlyMean.time, 'latitude': winds_monthlyMean.latitude, 
-                               'longitude': winds_monthlyMean.longitude})
-    return windEk
+    if save:
+        plt.savefig(savefig, dpi=600)
+    plt.show()
--- plot_topView_contourf.py
+++ plot_topView_contourf.py
@@ -1047,3 +1047,68 @@ def plotDataDensity_NIS_DIS(df1, df2, units='Data Density', save=False, savename
         return CF[0]
     else:
         plt.show();
+
+        
+
+def plot_fields_orthographic(field, longitude_coord, latitude_coord, vmin, vmax, save=False, savename="untitled.png",
+                             colorbar_label="", region_lons=None, mapax=None, plot_colorbar=True):
+    DATADIR = "/media/data"
+    passing_mapaxis = True
+    if not mapax:
+        passing_mapaxis = False
+        plt.close(1)
+        plt.figure(1, figsize=(7,7) )
+        gs = gridspec.GridSpec(5, 2, width_ratios=[1, 0.05], wspace=0.05)
+    
+        mapax = plt.subplot(gs[:,0], projection = ccrs.Orthographic(central_longitude=0, central_latitude=-90) )
+        
+    CF = mapax.pcolormesh(longitude_coord, latitude_coord, 
+                          field, transform=ccrs.PlateCarree(), cmap='RdBu_r', vmin = vmin, vmax = vmax)
+
+    gl = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, zorder=2,
+                  linewidth=0.5, color='gray', alpha=1, linestyle='--')
+    
+    gl.xlocator = mticker.FixedLocator(np.arange(-180, 181, 20))
+    gl.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5))
+    gl.xformatter = LONGITUDE_FORMATTER
+    gl.yformatter = LATITUDE_FORMATTER
+
+    if region_lons:
+        gl_regions = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle=':', zorder=3)
+        #gl_regions_lowZ = mapax.gridlines(crs=ccrs.PlateCarree(), draw_labels=False, linewidth=0.5, color='blue', alpha=1, linestyle='--', zorder=1)
+        gl_regions.xlocator = mticker.FixedLocator(region_lons)
+        gl_regions.ylocator = mticker.FixedLocator(np.arange(-80, -59, 5) )
+        gl_regions.xformatter = LONGITUDE_FORMATTER
+        gl_regions.yformatter = LATITUDE_FORMATTER
+        gl_regions.ylines = False
+
+    shpfile = DATADIR+"/Datasets/Shapefiles/AntarcticGroundingLine/GSHHS_f_L6.shp"
+    with fiona.open(shpfile) as records:
+        geometries = [sgeom.shape(shp['geometry']) for shp in records]
+    ISedgefname = DATADIR+"/Datasets/Shapefiles/AntIceShelf/ne_10m_antarctic_ice_shelves_polys.shp"
+    ISe_feature = ShapelyFeature(Reader(ISedgefname).geometries(), 
+                                 ccrs.PlateCarree(), linewidth=0.2,
+                                 facecolor='none', 
+                                 edgecolor="k")
+
+    mapax.add_geometries(geometries, ccrs.PlateCarree(), edgecolor='0.25', facecolor='0.7',alpha=0.25, linewidth=0.2)
+    mapax.add_feature(ISe_feature, zorder=3)
+
+    for l in np.arange(-160, 181, 20):
+        if( (l == 80) or (l == 100) ):
+            text_lat = -62.5
+        else:
+            text_lat  = -62.5
+        mapax.text(l, text_lat, str(l)+"$^{\circ}$", transform=ccrs.PlateCarree() )    
+
+    if not passing_mapaxis:
+        colorbar_ax = plt.subplot(gs[1:-1, 1])
+        Colorbar(mappable = CF, ax = colorbar_ax)
+        colorbar_ax.set_ylabel(colorbar_label)
+
+    if save:
+        plt.savefig(savefig, dpi=600)
+
+    if passing_mapaxis:
+        return CF
+    plt.show()
